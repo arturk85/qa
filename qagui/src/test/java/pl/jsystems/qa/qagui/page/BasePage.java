0bee782a039fb0d2ebbe07299689862e2e1c3506
arturk85 <a.krysiak@rolmar.pl>
2021-11-24 16:14:06 +0100

    selenium

---
+++ qagui/src/test/java/pl/jsystems/qa/qagui/page/BasePage.java
@@ -0,0 +1,34 @@
+package pl.jsystems.qa.qagui.page;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void waitForElement(By by, int maxTime){
+        Wait wait = new WebDriverWait(this.driver, Duration.ofSeconds(maxTime));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public void click(WebElement element, int maxTime){
+        waitForClickable(element,maxTime);
+        element.click();
+    }
+    public void waitForClickable(WebElement element, int maxTime){
+        Wait wait = new WebDriverWait(this.driver, Duration.ofSeconds(maxTime));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+}
